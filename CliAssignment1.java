3502b5c407dfac89943f9fa302f263edc757d5fc
Yashoda <gihany4@gmail.com>
2023-08-17 08:27:12 +0530

    ID adding done

--- CliAssignment1.java
+++ CliAssignment1.java
@@ -30,7 +30,63 @@ public class CliAssignment1 {
             final String APP_TITLE = String.format("%s%s%s",COLOR_BLUE_BOLD, screen, RESET);
             System.out.printf("\t%s\t\n", APP_TITLE);
 
-            
+            switch(screen){
+                case DASHBOARD:
+                System.out.println("\t[1]. Open New Account");
+                System.out.println("\t[2]. Deposit Money");
+                System.out.println("\t[3]. Withdraw Money");
+                System.out.println("\t[4]. Transfer Money");
+                System.out.println("\t[5]. Check Account Balance");
+                System.out.println("\t[6]. Drop Existing Account");
+                System.out.println("\t[7]. Exit");
+                System.out.print("\tEnter an option to continue: ");
+
+                int option = SCANNER.nextInt();
+                SCANNER.nextLine();
+
+                switch(option){
+                    case 1:
+                        screen = ADD_ACCOUNT;
+                        break;
+                    case 2:
+                        screen = DEPOSIT;
+                        break;
+                    case 3:
+                        screen = WITHDRAW;
+                        break;
+                    case 4:
+                        screen = TRANSFER;
+                        break;
+                    case 5:
+                        screen = PRINT_BALANCE;
+                        break;
+                    case 6:
+                        screen = DELETE_ACCOUNT;
+                        break;
+                    case 7:
+                        System.out.println(CLEAR);
+                        System.exit(0);
+                        default:
+                } break;
+
+                case ADD_ACCOUNT:
+                    int id = account.length;
+                    String name;
+                    int deposit;
+                    boolean valid;
+
+                    //ID 
+                    System.out.printf("\tNew account ID: SDB-%05d\n", (account.length + 1));
+                    int[] newId = new int[account.length+1];
+                    for (int i = 0; i < account.length; i++) {
+                        newId[i] = ID[i];
+                    }
+                    newId[newId.length - 1] = id;
+                    ID = newId;
+
+
+            }
+
         }while(true);
     }
 }
